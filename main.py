1b36c15d9cb6b8d95f4cf2e44dff17fc22b4611f
SaiHarshitha-192 <76095899+SaiHarshitha-192@users.noreply.github.com>
2023-07-30 20:02:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-
 import textbase
 from textbase.message import Message
 from textbase import models
 import os
-import requests
 from typing import List
+import random
+import requests
 from datetime import datetime, timedelta
 
 # Load your OpenAI API key
@@ -17,8 +17,28 @@ SYSTEM_PROMPT = """You are chatting with an AI. Feel free to ask me any question
 
 Additionally, I can provide you with the current weather information for any city or town. Just ask me something like 'What's the weather in London?' or 'Tell me the weather forecast for New York.' I'll fetch the latest weather data for you.
 
+I'm now equipped with a personal stylist feature too! If you need fashion advice, style tips, or want to know what's trending, just ask 'Can you be my personal stylist?' and I'll be happy to assist you with your fashion needs.
+
 If you ever want to end the conversation, simply say 'Goodbye' or 'Exit.' Now, let's have a pleasant chat!
 """
+
+# List of fashion advice/tips from the personal stylist
+FASHION_TIPS = [
+    "Monochrome outfits can create a sleek and sophisticated look.",
+    "Adding statement accessories can elevate a simple outfit.",
+    "Don't be afraid to mix patterns and textures to create an interesting outfit.",
+    "A well-fitted blazer can instantly polish any outfit.",
+    "Invest in a classic pair of jeans that flatters your body shape.",
+    "Neutral colors are versatile and can be easily mixed and matched.",
+    "Opt for quality over quantity when building your wardrobe.",
+    "Layering can add depth and dimension to your outfit.",
+    "Wearing the right undergarments can significantly improve the overall look of your outfit.",
+    "When in doubt, a little black dress is always a great choice.",
+]
+
+# State keys for tracking the conversation flow
+STATE_ASKING_PREFERENCE = "asking_preference"
+STATE_FASHION_TIP_GIVEN = "fashion_tip_given"
 
 
 @textbase.chatbot("talking-bot")
@@ -66,12 +86,29 @@ def on_message(message_history: List[Message], state: dict = None):
         else:
             return "Please provide a valid location for weather information.", state
 
+    # Check if the user asks for a personal stylist
+    if "can you be my personal stylist" in user_message:
+        state[STATE_ASKING_PREFERENCE] = True
+        return "Sure! I'd love to help you with some fashion advice. Let's get started! What's your favorite color?", state
+
+    # Handle the fashion preference questions and provide a fashion tip accordingly
+    if state.get(STATE_ASKING_PREFERENCE):
+        fashion_tip = get_fashion_tip_based_on_preference(user_message)
+        state[STATE_ASKING_PREFERENCE] = False
+        state[STATE_FASHION_TIP_GIVEN] = True
+        return fashion_tip, state
+
     # Generate GPT-3.5 Turbo response
     bot_response = models.OpenAI.generate(
         system_prompt=SYSTEM_PROMPT,
         message_history=message_history,
         model="gpt-3.5-turbo",
     )
+
+    # If a fashion tip has been given, continue the conversation without asking for preferences again
+    if state.get(STATE_FASHION_TIP_GIVEN):
+        state[STATE_FASHION_TIP_GIVEN] = False
+        return bot_response, state
 
     return bot_response, state
 
@@ -171,3 +208,8 @@ def get_coordinates(location):
         print(f"Error fetching location coordinates: {e}")
 
     return None, None
+
+
+def get_fashion_tip_based_on_preference(preference):
+    # Randomly select a fashion tip based on the user's preference
+    return random.choice(FASHION_TIPS)
